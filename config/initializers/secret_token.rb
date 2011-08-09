4cc54fb8bb08b4b995b8bb8aadb18475676d8364
Valeriy <invis89@gmail.com>
2011-08-09 17:42:10 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampeApp::Application.config.secret_token = '4aee28c0629321a4ef467e0d5b9db47806c1fbbe5ea6b062562594a714cdb2fb92b8ac1ab604ee3ab12dc2e74d036e86cfaeb46a39f77e60c38ab9c510ed9237'
