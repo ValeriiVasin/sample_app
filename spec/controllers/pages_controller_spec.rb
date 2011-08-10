6d945c127c0a9a56fd8673a4b00b0dcc62fb5831
Valeriy <invis89@gmail.com>
2011-08-11 00:03:57 +0300

    added last changes, writen 3 tests for title testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
+  before(:each) do
+    @base_title = 'My first app title | '
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector('title', :content => @base_title + "Home");
     end
   end
 
@@ -13,6 +23,23 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector('title', :content => @base_title + "Contact");
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector('title', :content => @base_title + "Help");
     end
   end
 
