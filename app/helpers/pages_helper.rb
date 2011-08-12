7244014fe7084cd9434699c8abf34a84bef1eabe
Valeriy Vasin <invis89@gmail.com>
2011-08-12 16:44:36 +0300

    removed pages helper - now we are using application helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,3 @@
 module PagesHelper
-
-  def title
-    if  @title.nil?
-        "Ruby on Rails Tutorial Sample App"
-    else
-        "Ruby on Rails Tutorial Sample App | #{@title}"
-    end
-  end
-
 end
 
