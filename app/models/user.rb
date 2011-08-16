bd83a86aeaf6ba39f4b464cd820f04e0bf23b38b
Valeriy Vasin <invis89@gmail.com>
2011-08-16 12:38:11 +0300

    added encryption with salt

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,12 +28,28 @@ class User < ActiveRecord::Base
                         
   before_save :encrypt_password
   
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
+  
   private
     def encrypt_password
-       self.encrypted_password = encrypt(password)
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
     end
-    
+
     def encrypt(string)
-      string  #temporary implementation
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
